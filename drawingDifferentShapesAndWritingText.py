c322e9bb54f4c14dc4a0e977b58f55cda3205d51
sonu shahuji <sonushahuji4@gmail.com>
2019-01-03 15:48:54 +0530

    commit

--- drawingDifferentShapesAndWritingText.py
+++ drawingDifferentShapesAndWritingText.py
@@ -12,7 +12,6 @@ img=cv2.imread('images/watch.png',cv2.IMREAD_COLOR)
 # Red => (0,0,255)
 # Black => (0,0,0)
 # White => (255,255,255)
-'''
 
 cv2.line(img,(60,10),(60,100),(255,0,0),15)
 cv2.imshow('image',img)
@@ -21,17 +20,17 @@ cv2.destroyAllWindows()
 cv2.imwrite('output/line.png',img)
 
 # output is in directory "output" =.line.png
-'''
+
 
 # Drawing a rectange on the image using cv2
 # cv2.reactangle(image,start from(x,y),end at(x,y),color,thickness)
 
-# cv2.rectangle(img,(20,400),(340,100),(0,255,0),5)
-# cv2.imshow('rectangle',img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-# cv2.imwrite('output/line.png',img)
-# cv2.imwrite('output/reactangle.png',img)
+cv2.rectangle(img,(20,400),(340,100),(0,255,0),5)
+cv2.imshow('rectangle',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imwrite('output/line.png',img)
+cv2.imwrite('output/reactangle.png',img)
 
 # cv2.circle(image,start from(x,y),radius,color,thickness)
 cv2.circle(img,(175,245),140,(0,0,255),5)
