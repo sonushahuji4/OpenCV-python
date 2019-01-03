20374a07cf1ddb83837660c25e7ae6f3f5cce795
sonu shahuji <sonushahuji4@gmail.com>
2019-01-03 12:39:48 +0530

    Loading images using OpenCV-python

---
+++ loadingImages.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# video is measured frame per second and each frame is image
+
+'''
+---------------Note------------
+IMREAD_GRAYSCALE = 0
+IMREAD_COLOR = 1
+IMREAD_UNCHANGED = -1
+'''
+'''
+There are various way of showing the images like...
+1) by matplotlib
+2) opencv 
+'''
+'''
+Showing images using OpenCV
+'''
+img=cv2.imread('images/watch.jpg',cv2.IMREAD_GRAYSCALE)
+cv2.imshow('image',img) # To display image data, use the imshow function
+cv2.waitKey(0)  # Waits for a pressed key
+cv2.destroyAllWindows() # The function destroyAllWindows destroys all of the opened HighGUI windows
