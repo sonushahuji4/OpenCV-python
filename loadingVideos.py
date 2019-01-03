2ff172383836f002e30f9c5f19d36ee0c2545cfe
sonu shahuji <sonushahuji4@gmail.com>
2019-01-03 14:33:56 +0530

    Loading multiple Video Camera

--- loadingVideos.py
+++ loadingVideos.py
@@ -4,8 +4,9 @@ import numpy as np
 cap=cv2.VideoCapture(0)
 while True:
     ret, frame=cap.read()
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     cv2.imshow('frame',frame)   # this is color frame
-
+    cv2.imshow('gray',gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
