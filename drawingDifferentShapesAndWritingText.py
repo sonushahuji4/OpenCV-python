e9d05a71bb715d2e3fbeb465d94cd8cf37484bd6
sonu shahuji <sonushahuji4@gmail.com>
2019-01-03 15:43:21 +0530

    drawing circl and saving it in output directory

--- drawingDifferentShapesAndWritingText.py
+++ drawingDifferentShapesAndWritingText.py
@@ -24,11 +24,19 @@ cv2.imwrite('output/line.png',img)
 '''
 
 # Drawing a rectange on the image using cv2
-# cv2.reactangle(image,start from(x,y),end at(x,y),color,thickness,)
+# cv2.reactangle(image,start from(x,y),end at(x,y),color,thickness)
 
-cv2.rectangle(img,(20,400),(340,100),(0,255,0),5)
+# cv2.rectangle(img,(20,400),(340,100),(0,255,0),5)
+# cv2.imshow('rectangle',img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+# cv2.imwrite('output/line.png',img)
+# cv2.imwrite('output/reactangle.png',img)
+
+# cv2.circle(image,start from(x,y),radius,color,thickness)
+cv2.circle(img,(175,245),140,(0,0,255),5)
 cv2.imshow('rectangle',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 cv2.imwrite('output/line.png',img)
-cv2.imwrite('output/reactangle.png',img)
+cv2.imwrite('output/circle.png',img)
