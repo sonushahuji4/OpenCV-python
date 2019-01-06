42da305b0d4b3426bfa4dabf13e8c2cdf7b90dca
sonu shahuji <sonushahuji4@gmail.com>
2019-01-06 13:40:44 +0530

    Edge Detection

--- colorFilteringOpenCV.py
+++ colorFilteringOpenCV.py
@@ -6,8 +6,8 @@ while True:
     _, frame = cap.read()
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lower_red = np.array([100,100,100])
-    upper_red = np.array([255,255,255])
+    lower_red = np.array([90,90,90])
+    upper_red = np.array([250,250,250])
     mask = cv2.inRange(hsv, lower_red, upper_red)
     res = cv2.bitwise_and(frame, frame, mask = mask)
 
